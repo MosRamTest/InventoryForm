211776de446d9858c80af38e8e2665d38f5a8dbb
MosRamTest <mosram2@gmail.com>
2025-09-21 15:15:09 +0200

    Created method to verify that homepage is displayed

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -1,4 +1,21 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+
 public class HomePage {
+    WebDriver driver;
+    @FindBy(id = "overview-hero")
+     WebElement homePageTitle_id;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void verifyHomePageIsDisplayed(WebElement homePageTitle_id) {
+        this.homePageTitle_id = homePageTitle_id;
+    }
 }
